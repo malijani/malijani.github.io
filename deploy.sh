68b64bdb9fa3a927f24e8189d109341838aa9d51
malijani <alijanialijanvandmohammad@gmail.com>
2020-03-04 18:04:37 +0330

    Deployed ; Updated website

---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# script to auto update master branch of my website hosted on github
+
+# check if local webserver is already up to download local version
+if [[ $(lsof -Pi :8000 -sTCP:LISTEN) ]]; then
+    echo "Local site is up! start to deploy."
+else
+    echo "localhost:8000 is down! Aborting"
+    exit 1
+fi
+
+# remove all files except these:
+shopt -s extglob
+rm -rv !(".gitignore"|"CNAME"|".git"|".nojekyll"|".venv"|".vscode"|"deploy.sh"|"src")
+shopt -u extglob
+
+# download website from localhost:8000
+wget -mkEpnp localhost:8000
+
+# move files from site directory
+mv localhost:8000/* .
+
+# remove unneeded directory
+rm -rfv localhost:8000
+
+# add files to git
+git add .
+
+# commit site update
+COMMIT=$(git rev-parse --short HEAD)
+git commit -m "Deployed ${COMMIT}; Updated website"
+
+# push changes
+git push -u origin master
+
+
+
+
